81f8a0836baf0d93970483f6ef2874098d34ec9a
Stefano Reynard <stefanoreynardqn@gmail.com>
2023-12-14 23:59:28 -0300

    Presupuestos

--- js/script.js
+++ js/script.js
@@ -1,37 +1,30 @@
 function obtenerProductosDisponibles(presupuestoDisponible) {
-    const precios = {
-      "VW Vento 2015": 12500,
-      "VW Gol 2017": 9500,
-      "VW Amarok 0km": 25000,
-      "VW GTI 2016": 33200,
-      "VW Polo Track 2022": 15000
-    };
+  const productos = [
+    { modelo: "VW Vento 2015", precio: 12500 },
+    { modelo: "VW Gol 2017", precio: 9500 },
+    { modelo: "VW Amarok 0km", precio: 25000 },
+    { modelo: "VW GTI 2016", precio: 33200 },
+    { modelo: "VW Polo Track 2022", precio: 15000 }
+  ];
 
-    let productosDisponibles = [];
+  const productosDisponibles = productos
+    .filter(producto => producto.precio <= presupuestoDisponible)
+    .map(producto => producto.modelo);
 
-    for (const producto in precios) {
-      if (precios.hasOwnProperty(producto)) {
-        if (precios[producto] <= presupuestoDisponible) {
-          productosDisponibles.push(producto);
-        }
-      }
-    }
+  return productosDisponibles;
+}
 
-    return productosDisponibles;
-  }
+let presupuestoUsuario = prompt("Ingresa tu presupuesto disponible:");
+let presupuestoDisponible = parseFloat(presupuestoUsuario);
 
-  let presupuestoUsuario = prompt("Ingresa tu presupuesto disponible:");
-  let presupuestoDisponible = parseFloat(presupuestoUsuario);
+if (!isNaN(presupuestoDisponible)) {
+  let productosDisponibles = obtenerProductosDisponibles(presupuestoDisponible);
 
-  if (!isNaN(presupuestoDisponible)) {
-
-    let productosDisponibles = obtenerProductosDisponibles(presupuestoDisponible);
-
-    if (productosDisponibles.length > 0) {
-      alert(`Los productos disponibles dentro de tu presupuesto son: ${productosDisponibles.join(", ")}`);
-    } else {
-      alert("Lo siento, no hay productos disponibles dentro de tu presupuesto.");
-    }
+  if (productosDisponibles.length > 0) {
+    alert(`Los productos disponibles dentro de tu presupuesto son: ${productosDisponibles.join(", ")}`);
   } else {
-    alert("Por favor, ingresa un presupuesto válido.");
+    alert("Lo siento, no hay productos disponibles dentro de tu presupuesto.");
   }
+} else {
+  alert("Por favor, ingresa un presupuesto válido.");
+}
